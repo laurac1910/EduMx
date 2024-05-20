9d7db7861588d9176a79f7cefaa66689ba72c9ac
laurac1910 <lauracordero0806@gmail.com>
2024-05-19 20:57:31 -0700

    8

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import HomeThree from "./Components/Home/HomeThree";
 import ErrorPage from "./Components/Pages/404Page";
 import AboutTwo from "./Components/Pages/AboutTwo";
